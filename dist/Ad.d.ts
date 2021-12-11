c9999e13e891663eb810ccae8e1fbea78312f67a
Wellington Morete <welllington.morete@gmail.com>
2021-12-10 21:29:03 -0300

    Adiciona o tipo URL para link

--- dist/Ad.d.ts
+++ dist/Ad.d.ts
@@ -2,7 +2,7 @@ import { Property } from "./Property";
 import { TypeAd } from "./TypeAd";
 export declare type Ad = {
     price: number | string;
-    link: string;
+    link: string | URL;
     typeAd: TypeAd;
     property: Property;
 };
