eaa941a85083313b7756efb099c046cec76d6731
Wellington Morete <welllington.morete@gmail.com>
2021-12-18 18:59:15 -0300

    Update: Adiciona a propriedade suíte nas características principais

--- dist/CharacteristicMain.d.ts
+++ dist/CharacteristicMain.d.ts
@@ -1,7 +1,7 @@
 export declare enum CharacteristicMain {
-   BEDROOM = "quarto",
-   PARKING_SPACE = "vaga",
-   BATHROOM = "banheiro",
-   AREA = "area",
-   SUITE = "su\u00EDte"
+    BEDROOM = "quarto",
+    PARKING_SPACE = "vaga",
+    BATHROOM = "banheiro",
+    AREA = "area",
+    SUITE = "su\u00EDte"
 }
--- dist/CharacteristicMain.js
+++ dist/CharacteristicMain.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CharacteristicMain = void 0;
 var CharacteristicMain;
 (function (CharacteristicMain) {
-   CharacteristicMain["BEDROOM"] = "quarto";
-   CharacteristicMain["PARKING_SPACE"] = "vaga";
-   CharacteristicMain["BATHROOM"] = "banheiro";
-   CharacteristicMain["AREA"] = "area";
-   CharacteristicMain["SUITE"] = "su\u00EDte";
+    CharacteristicMain["BEDROOM"] = "quarto";
+    CharacteristicMain["PARKING_SPACE"] = "vaga";
+    CharacteristicMain["BATHROOM"] = "banheiro";
+    CharacteristicMain["AREA"] = "area";
+    CharacteristicMain["SUITE"] = "su\u00EDte";
 })(CharacteristicMain = exports.CharacteristicMain || (exports.CharacteristicMain = {}));
